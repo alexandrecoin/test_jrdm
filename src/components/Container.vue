942d2bd97cea13b7edd78c363d27ea5b2c93d250
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-06-03 18:46:09 +0200

    Added Bootstrap vue

---
+++ src/components/Container.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    Container Working
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+ 
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
