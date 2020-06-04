3901340724646dca7c4b33e024c6451afd142782
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-06-04 08:11:32 +0200

    Added basic elevator calling system

---
+++ src/components/Building.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="floor in floors" :key="floor">
+          <td>{{ floor }}</td>
+          <td>
+            <b-button/>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['floors'],
+};
+</script>
---
+++ src/components/Panel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <tbody>
+      <tr v-for="floor in floors" :key="floor">
+        <td>
+          <b-button @click="callElevator(floor)">
+            {{ floor }}
+          </b-button>
+        </td>
+      </tr>
+    </tbody>
+    <b-row>
+      <b-col>
+        <b-button disabled="disabled">{{
+          this.isSelectedFloorUp ? 'UP' : 'DOWN'
+        }}</b-button>
+      </b-col>
+      <b-col>
+        <b-button disabled="disabled">
+          {{ this.isDoorOpen ? 'OPEN' : 'CLOSED' }}
+        </b-button>
+      </b-col>
+    </b-row>
+    <p>You are at floor : {{ currentFloor }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['floors', 'currentFloor', 'isDoorOpen', 'isSelectedFloorUp'],
+  data() {
+    return {
+      disabled: true,
+    };
+  },
+  methods: {
+    callElevator(floor) {
+      this.$emit('callElevator', floor);
+    },
+  },
+};
+</script>
